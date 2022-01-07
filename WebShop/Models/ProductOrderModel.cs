6677147db58505159c19c859361b4dab21edf019
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2022-01-07 11:09:33 +0100

    Added database tabels with EntityFramework. Database in GearHost.

---
+++ WebShop/Models/ProductOrderModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.Models
+{
+    public class ProductOrderModel
+    {
+        public int ProductId { get; set; }
+        public ProductModel Product { get; set; }
+
+        public int OrderId { get; set; }
+
+        public OrderModel Order { get; set; }
+    }
+}
