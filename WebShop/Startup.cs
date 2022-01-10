20a77b36deed0c26fa71ca96ff79d2bdce6eb9dd
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2022-01-10 15:07:00 +0100

    Edited password requirements. Added home button.

--- WebShop/Startup.cs
+++ WebShop/Startup.cs
@@ -35,6 +35,16 @@ namespace WebShop
 
             services.AddRazorPages();
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
