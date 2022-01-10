9df9191f00493be13a66ead7e53d2666efcd2fb6
Daniel Alexandre <dani.alex@live.se>
2022-01-10 13:27:18 +0100

    kind of done, some testing wanted

---
+++ WebShop/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
