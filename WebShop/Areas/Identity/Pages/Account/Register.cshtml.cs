4990f298c1eb18fe74f2ddf1005a99433d81b224
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2022-01-14 20:10:04 +0100

    Added fields to registration. Updated database.

--- WebShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using WebShop.Models;
 
 namespace WebShop.Areas.Identity.Pages.Account
 {
@@ -51,6 +52,31 @@ namespace WebShop.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [StringLength(50)]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            [Display(Name = "Last name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            [Display(Name = "Street")]
+            public string Street { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            [Display(Name = "Zip Code")]
+            public string ZipCode { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            [Display(Name = "City")]
+            public string City { get; set; }
+
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -74,7 +100,13 @@ namespace WebShop.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    Street = Input.Street,
+                    ZipCode = Input.ZipCode,
+                    City = Input.City,
+                    Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
