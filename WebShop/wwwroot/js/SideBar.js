2763f6e753398683b2e48d6223e0d7172110593c
madiha gul <mmadihagull@gmail.com>
2022-01-19 09:39:29 +0100

    Cart

--- WebShop/wwwroot/js/SideBar.js
+++ WebShop/wwwroot/js/SideBar.js
@@ -1,20 +1,9 @@
-﻿CartPostBackURL = '/Home/GetCarttInfo';
+﻿
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
-        $.ajax({
-            type: "GET",
-            url: CartPostBackURL,
-            contentType: "application/json; charset=utf-8",
-            datatype: "json",
-            success: function (data) {
-                $('#cartInfo').html(data);
-            },
-            error: function () {
-                alert("Dynamic content load failed.");
-            }
-        });
+   
 
-        //$('#sidebar').toggleClass('active');
-        //$('#content').toggleClass('active');
+        $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
     });
 });
