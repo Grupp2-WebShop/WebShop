2d6ee7e8ee139c430562a01ff0e957b548041df2
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2022-01-20 10:57:34 +0100

    Reset to previous

--- WebShop/wwwroot/js/SideBar.js
+++ WebShop/wwwroot/js/SideBar.js
@@ -1,8 +1,5 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
-   
-
         $('#sidebar').toggleClass('active');
         $('#content').toggleClass('active');
     });
