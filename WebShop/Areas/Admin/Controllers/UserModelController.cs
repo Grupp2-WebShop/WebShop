5305abd43b133732885d6a4f6740fb95803c6a30
Daniel Alexandre <dani.alex@live.se>
2022-01-21 10:13:07 +0100

    Add and remove user functio for admin

---
+++ WebShop/Areas/Admin/Controllers/UserModelController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Models;
+
+namespace WebShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserModelController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserModelController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<ApplicationUser> list = _context.Users.ToList();
+            return View(list);
+        }
+
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(ApplicationUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userModel = await _context.Users.FindAsync(id);
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+            return View(userModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Email,Password,FirstName,City")] ApplicationUser userModel)
+        {
+            if (id != userModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!userModelExists(userModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userModel);
+        }
+
+        private bool userModelExists(string id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userModel = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var userModel = await _context.Users.FindAsync(id);
+            _context.Users.Remove(userModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
