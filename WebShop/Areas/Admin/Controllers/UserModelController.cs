1626ef230f7d4173179caffe868929e457cfe9c3
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2022-01-25 09:27:31 +0100

    Fixed saving of Email when editing user.

--- WebShop/Areas/Admin/Controllers/UserModelController.cs
+++ WebShop/Areas/Admin/Controllers/UserModelController.cs
@@ -81,8 +81,8 @@ namespace WebShop.Areas.Admin.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Id,City,FirstName, LastName,Street,ZipCode,City,Discriminator,EmailConfirmed,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnabled," +
-            "AccessFailedCoun,UserName,NormalizedUserName,NormalizedEmail,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,LockoutEnd ")] ApplicationUser userModel)
+        public async Task<IActionResult> Edit(string id, [Bind("Id,City,FirstName,LastName,Street,ZipCode,City,Discriminator,Email,EmailConfirmed,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnabled," +
+            "AccessFailedCoun,UserName,NormalizedUserName,NormalizedEmail,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,LockoutEnd")] ApplicationUser userModel)
         {
             if (id != userModel.Id)
             {
