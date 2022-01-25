c4187f0608df0c93b3bdf5e370fb3f2c72ce7af1
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2022-01-25 21:38:27 +0100

    Fixed delete hole Order.

--- WebShop/Areas/Admin/Controllers/OrderModelController.cs
+++ WebShop/Areas/Admin/Controllers/OrderModelController.cs
@@ -47,17 +47,48 @@ namespace WebShop.Areas.Admin.Controllers
             return View(choosenOrders);
         }
 
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> DeleteOrder(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
+            List<ApplicationUser> users = _context.Users.ToList();
+            var choosenOrder = await _context.Order.FirstOrDefaultAsync(m => m.OrderId == id);
+            if (choosenOrder == null)
+            {
+                return NotFound();
+            }
+
+            return View(choosenOrder);
+        }
+
+        public async Task<IActionResult> DeleteOrderConfirmed(int id)
+        {
+            var order = await _context.Order.FirstOrDefaultAsync(m => m.OrderId == id);
+            _context.Order.Remove(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? orderId, int productId)
+        {
+            if (orderId == null)
+            {
+                return NotFound();
+            }
+
             List<ProductModel> products = _context.Product.ToList();
             List<ApplicationUser> users = _context.Users.ToList();
-            List<OrderModel> orders = _context.Order.Where(p => p.OrderId == id).ToList();
-            var choosenOrder = await _context.ProductOrder.FirstOrDefaultAsync(m => m.ProductId == id);
+            List<OrderModel> orders = _context.Order.Where(p => p.OrderId == orderId).ToList();
+            List<ProductOrderModel> choosenOrders = _context.ProductOrder.Where(p => p.OrderId == orderId).ToList();
+
+            //var choosenOrder = choosenOrders.Where(p => p.ProductId == orderId);
+
+            var choosenOrder = await _context.ProductOrder.FirstOrDefaultAsync(m => m.ProductId == productId);
+            //choosenOrder.Order = choosenOrders.Where(p => p.ProductId == id);
+
             if (choosenOrder == null)
             {
                 return NotFound();
