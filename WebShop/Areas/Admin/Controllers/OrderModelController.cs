c706a6fe5b0654886060e496fe91c7bc08237d93
Tomas Varo <42610038+TomasVaro@users.noreply.github.com>
2022-01-25 13:03:42 +0100

    Working on Delete order

--- WebShop/Areas/Admin/Controllers/OrderModelController.cs
+++ WebShop/Areas/Admin/Controllers/OrderModelController.cs
@@ -16,6 +16,7 @@ namespace WebShop.Areas.Admin.Controllers
         {
             _context = context;
         }
+
         public async Task<IActionResult> Index()
         {
             List<ApplicationUser> users = _context.Users.ToList();
@@ -24,6 +25,7 @@ namespace WebShop.Areas.Admin.Controllers
             //List<ProductOrderModel> productOrder = _context.ProductOrder.ToList();
             return View(await _context.Order.ToListAsync());
         }
+
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -31,40 +33,47 @@ namespace WebShop.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            List<ProductOrderModel> productOrders = _context.ProductOrder.ToList();
             var productOrder = await _context.ProductOrder.FirstOrDefaultAsync(p => p.OrderId == id);
-
-            List<ProductModel> products = _context.Product.ToList();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            var product = await _context.Product.FirstOrDefaultAsync(p => p.ProductId == productOrder.ProductId);
-
-            List<OrderModel> orders = _context.Order.ToList();
-            var order = await _context.Order.FirstOrDefaultAsync(p => p.OrderId == productOrder.OrderId);
-
-            List<ApplicationUser> users = _context.Users.ToList();
             if (productOrder == null)
             {
                 return NotFound();
             }
 
-            return View(productOrder);
+            List<ProductModel> products = _context.Product.ToList();
+            List<ApplicationUser> users = _context.Users.ToList();
+            List<OrderModel> orders = _context.Order.Where(p => p.OrderId == id).ToList();
+            List<ProductOrderModel> choosenOrders = _context.ProductOrder.Where(p => p.OrderId == id).ToList();
+
+            return View(choosenOrders);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            List<ProductModel> products = _context.Product.ToList();
+            List<ApplicationUser> users = _context.Users.ToList();
+            List<OrderModel> orders = _context.Order.Where(p => p.OrderId == id).ToList();
+            var choosenOrder = await _context.ProductOrder.FirstOrDefaultAsync(m => m.ProductId == id);
+            if (choosenOrder == null)
+            {
+                return NotFound();
+            }
+
+            return View(choosenOrder);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.ProductOrder.FindAsync(id);
+            _context.ProductOrder.Remove(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
